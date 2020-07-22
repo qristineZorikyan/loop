e56dae33ed2fd87dbd93caa6054a51c639d75018
qristineZorikyan <63978800+qristineZorikyan@users.noreply.github.com>
2020-07-22 17:39:47 +0400

    loop

---
+++ index.js
@@ -0,0 +1,28 @@
+var x,y,chr;
+for(x=1; x <=6; x++)
+{
+   for (y=1; y < x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+
+
+
+
+var x, y, star;
+for(x=6; x>y; x-- )
+{
+	for(y=6; y>x; y--)
+		{
+			star=star+("*");
+		}
+	console.log(star);
+	star="";
+}
+
+
+
+
